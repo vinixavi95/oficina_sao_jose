689385df99820e29064473750ec97922763c7a8a
vinixavi95 <vinixavi95@gmail.com>
2024-01-24 15:10:59 -0300

    Configurando solid js

---
+++ dist/frontend/vite.config.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = require("@solidjs/start/config");
+exports.default = (0, config_1.defineConfig)({});
--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = 5000;
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript with Express!');
 });
